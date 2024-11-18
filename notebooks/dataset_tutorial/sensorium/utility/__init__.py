c7c923c2600a85c2be1393a424699c49ec69a730
Nisone2000 <136569787+Nisone2000@users.noreply.github.com>
2024-11-18 15:32:24 +0100

    Delete notebooks/dataset_tutorial/sensorium directory
    Is not needed

--- notebooks/dataset_tutorial/sensorium/utility/__init__.py
+++
@@ -1,3 +0,0 @@
-from .eval import evaluate, load_groundtruth_data, load_submission_data
-from .scores import get_correlations, get_fev, get_signal_correlations
-from .submission import generate_ground_truth_file, generate_submission_file
